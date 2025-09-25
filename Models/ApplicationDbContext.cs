ef45b849d9b5cae454698af7bbde90374c10ab2f
vega0 <vega0@DESKTOP-UDTT89S>
2025-09-25 12:58:35 -0600

    se crea modelo y conexion con la base de datos

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppFormMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+
+    }
+}
